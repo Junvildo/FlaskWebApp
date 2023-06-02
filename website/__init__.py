ad3501edf1a0c5de9dfee688d02b37d7815b3d17
Junvildo <tienloc317@gmail.com>
2023-06-02 15:28:49 +0700

    Finish

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
+
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +13,8 @@ def create_app():
     app.config['SECRET_KEY'] = 'lmao'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(DB_NAME)
     db.init_app(app)
+
+
 
     from .views import views
     from .auth import auth
@@ -21,5 +26,14 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
